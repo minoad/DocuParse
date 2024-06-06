378390867f598e4a9aa11a2572f91dd71c6063da
Micah Norman <minoad@gmail.com>
2024-06-06 17:34:16 -0500

    establish

---
+++ main.py
@@ -0,0 +1,25 @@
+#!./venv/bin/python
+
+"""
+Module Docstring
+"""
+import sys
+
+from docuparse import config, logger
+
+
+def main() -> int:
+    """
+    main thread
+
+    Returns:
+        0 int if good.  int > 0 if some error.
+    """
+    logger.debug("begin run")
+    logger.debug(config)
+    print("testing")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ tests/test_pdf.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import pytest
+
+from docuparse import config, logger
+
+test_cases: dict[str, dict[str, Any]] = {
+    "invalid_path": {
+        "function": lambda: config.ENVIRONMENT,
+        "expected": "dev",
+        "expected_exception": None,
+    },
+}
+
+
+@pytest.mark.parametrize("name, case", test_cases.items())
+def test_pdfs(name, case):
+    """
+    Generic test cases
+    """
+    func = case["function"]
+
+    # Handle cases where input might be missing or incorrect
+    if case.get("expected_exception", None):
+        with pytest.raises(case["expected_exception"]):
+            result = func()
+    else:
+        result = func()
+        logger.debug("%s == %s", result, case["expected"])
+        assert result == case["expected"], f"Test {name} failed: expected {case['expected']} but got {result}"
+
+
+if __name__ == "__main__":
+    pytest.main()
